98d8b9ac1793bcf1d1e5f17111558104ec66527e
CodeLazarus <draganstamenkovic@outlook.com>
2025-06-07 12:49:29 +0200

    added level and ship scriptable object registrator

--- Assets/Scripts/Registrators/DataRegistrator.cs
+++ Assets/Scripts/Registrators/DataRegistrator.cs
@@ -1,5 +1,6 @@
 using Data.Load;
 using Data.Save;
+using UnityEngine;
 using VContainer;
 
 namespace Registrators
@@ -10,6 +11,23 @@ namespace Registrators
         {
             builder.Register<ISaveManager, SaveManager>(Lifetime.Singleton);
             builder.Register<ILoadManager, LoadManager>(Lifetime.Singleton);
+            
+            var levels = Resources.LoadAll<ScriptableObject>("Data/Levels");
+            foreach (var level in levels)
+            {
+                Debug.Log($"Registering data: {level.name} ({level.GetType()})");
+                builder.RegisterInstance(level)
+                    .As(level.GetType())
+                    .AsImplementedInterfaces();
+            }
+            var ships = Resources.LoadAll<ScriptableObject>("Data/Ships");
+            foreach (var ship in ships)
+            {
+                Debug.Log($"Registering data: {ship.name} ({ship.GetType()})");
+                builder.RegisterInstance(ship)
+                    .As(ship.GetType())
+                    .AsImplementedInterfaces();
+            }
         }
     }
 }
