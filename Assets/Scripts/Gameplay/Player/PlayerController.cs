01052a2eb8ebe74509f022030dd0837b5b8f7caf
CodeLazarus <draganstamenkovic@outlook.com>
2025-06-19 14:43:35 +0200

    Implemented movement boundries to player

--- Assets/Scripts/Gameplay/Player/PlayerController.cs
+++ Assets/Scripts/Gameplay/Player/PlayerController.cs
@@ -14,6 +14,7 @@ namespace Gameplay.Player
         private readonly IObjectResolver _objectResolver;
 
         private PlayerView _playerView;
+        private Camera _camera;
 
         public PlayerController(IObjectResolver objectResolver)
         {
@@ -26,6 +27,8 @@ namespace Gameplay.Player
                 .GetActiveShipPrefab(_gameData.ActiveShip.Id), parent);
             
             _playerView = player.GetComponent<PlayerView>();
+            //TODO: Maybe create CameraManager cause it will needed for more advanced stuff
+            _camera = Camera.main;
             
             SetActive(false);
         }
@@ -44,9 +47,10 @@ namespace Gameplay.Player
             _playerView.Rigidbody.linearVelocity = velocity;
         }
 
-        public void Move(Vector2 velocity)
+        public void Move(Vector2 direction)
         {
-            _playerView.Rigidbody.linearVelocity = velocity * _playerConfig.speed;
+            var velocity = ApplyBoundaryDampening(direction * _playerConfig.speed);
+            _playerView.Rigidbody.linearVelocity = velocity;
         }
 
         public void SetActive(bool active)
@@ -62,6 +66,54 @@ namespace Gameplay.Player
                 _playerView.Rigidbody.linearVelocity = Vector2.zero;
                 _playerView.transform.localPosition = _playerConfig.startPosition;
             }
+        }
+        
+        
+        //TODO: refactor this to store values at the Initialization rather on each update method
+        Vector2 ApplyBoundaryDampening(Vector2 velocity)
+        {
+            var pos = _playerView.transform.position;
+    
+            // Get camera bounds
+            var camHeight = _camera.orthographicSize;
+            var camWidth = camHeight * _camera.aspect;
+            var camPos = _camera.transform.position;
+
+            var playerScaleX = _playerView.transform.localScale.x / 2;
+
+            var minX = camPos.x - camWidth + playerScaleX;
+            var maxX = camPos.x + camWidth - playerScaleX;
+            //var minY = camPos.y - camHeight;
+            //var maxY = camPos.y + camHeight;
+    
+            // Dampening distance from edge
+            var dampeningZone = 0.1f;
+    
+            // Dampen X velocity near boundaries
+            if (pos.x <= minX + dampeningZone && velocity.x < 0)
+            {
+                var factor = (pos.x - minX) / dampeningZone;
+                velocity.x *= Mathf.Clamp01(factor);
+            }
+            else if (pos.x >= maxX - dampeningZone && velocity.x > 0)
+            {
+                var factor = (maxX - pos.x) / dampeningZone;
+                velocity.x *= Mathf.Clamp01(factor);
+            }
+            /* Vertical boundary dampening not needed now
+                    // Dampen Y velocity near boundaries
+                    if (pos.y <= minY + dampeningZone && velocity.y < 0)
+                    {
+                        var factor = (pos.y - minY) / dampeningZone;
+                        velocity.y *= Mathf.Clamp01(factor);
+                    }
+                    else if (pos.y >= maxY - dampeningZone && velocity.y > 0)
+                    {
+                        var factor = (maxY - pos.y) / dampeningZone;
+                        velocity.y *= Mathf.Clamp01(factor);
+                    }
+            */
+            return velocity;
         }
     }
 }
